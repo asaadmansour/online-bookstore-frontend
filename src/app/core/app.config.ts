1838fb523b9577d91e861209a5328a22f53d958b
asaadmansour <as89aadmansour@gmail.com>
2026-03-01 00:55:20 +0200

    added the live backend url

--- src/app/core/app.config.ts
+++ src/app/core/app.config.ts
@@ -2,5 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { API_URL } from './api.config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [{ provide: API_URL, useValue: 'http://localhost:5000/' }],
+  providers: [
+    {
+      provide: API_URL,
+      useValue: 'https://online-bookstore-backend-production-4861.up.railway.app/',
+    },
+  ],
 };
